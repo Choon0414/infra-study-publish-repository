33aeef7790795aa92fc19677632d7b4fef566a18
춘식 <142300831+Choon0414@users.noreply.github.com>
2024-07-15 23:02:32 +0900

    entity DB와 연동

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -1,14 +1,27 @@
 package roomescape.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
+
 @Getter
 @Builder
+@Table(name = "reservation")
 public class Reservation {
 
+    @Id
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "date")
     private String date;
+
+    @Column(name = "time")
     private Time time;
 
     public Reservation(Long id, String name, String date, Time time){
--- src/main/java/roomescape/domain/Time.java
+++ src/main/java/roomescape/domain/Time.java
@@ -1,13 +1,21 @@
 package roomescape.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@Table(name = "time")
 public class Time {
 
+    @Id
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "time")
     private String time;
 
     public Time(Long id, String time){
